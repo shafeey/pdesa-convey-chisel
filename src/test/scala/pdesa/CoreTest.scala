c283c7941f79ca0711fa8bf2df6f49b5a8c71caf
Shafiur Rahman <shafeeyrahman@gmail.com>
2017-07-27 21:42:04 -0700

    Add more test cases
    Additional test cases for:
    - Rollback events
    - Anti-messages
    - Expired history message

--- src/test/scala/pdesa/CoreTest.scala
+++ src/test/scala/pdesa/CoreTest.scala
@@ -92,16 +92,40 @@ class CoreTesterBase(c: PDESACore) extends AdvTester(c) {
     rd_req
   }
 
-  protected def sendHistMsg(req: HistReq, msg: HistMsg) = {
+  protected def sendHistMsg(req: HistReq, msg: HistMsg): Unit = {
     val rsp = HistRsp(req.cid, req.op, msg)
     sendHist(rsp)
   }
 
-  protected def waitForGenEvt(time: BigInt): EventData = {
+  protected def sendHistMsg(req: HistReq): Unit = {
+    /* Write acknowlegement */
+    sendHistMsg(req, HistMsg(0, 0, 0, 0))
+  }
+
+  private def waitForEvent(): EventData = {
     eventually(gen_evt_handler.outputs.nonEmpty)
     val gen_evt: EventData = gen_evt_handler.outputs.dequeue()
+    gen_evt
+  }
+
+  protected def waitForGenEvt(time: BigInt): EventData = {
+    val gen_evt = waitForEvent()
     expect(gen_evt.cancel == 0, "Received event type not correct")
     expect(gen_evt.time > time, "Received event time is not valid")
+    gen_evt
+  }
+
+  protected def waitForRollbackEvt(time: BigInt, lp: BigInt): EventData = {
+    val gen_evt = waitForEvent()
+    expect(gen_evt.cancel == 0, "Received event type not correct")
+    expect(gen_evt.time == time, "Received event time is not valid")
+    gen_evt
+  }
+
+  protected def waitForCancelEvt(time: BigInt): EventData = {
+    val gen_evt = waitForEvent()
+    expect(gen_evt.cancel == 1, "Received event type not correct")
+    expect(gen_evt.time == time, "Cancellation event time is not valid")
     gen_evt
   }
 
@@ -115,6 +139,8 @@ class CoreTesterBase(c: PDESACore) extends AdvTester(c) {
   protected def waitForEvtCompletion() = {
     eventually(evt_complete_handler.outputs.nonEmpty)
     expect(c.io.done.valid, 1, "Event completion signal not received")
+    expect(hist_req_handler.outputs.isEmpty, "Unwanted pending history request.")
+    expect(gen_evt_handler.outputs.isEmpty, "Unwanted pending event message.")
   }
 
   protected def waitUntilEvtReq() = {
@@ -123,18 +149,19 @@ class CoreTesterBase(c: PDESACore) extends AdvTester(c) {
 }
 
 //noinspection ScalaStyle:magic.number
-class SimpleCoreTester(c: PDESACore) extends CoreTesterBase(c) {
+class CoreSimpleTester(c: PDESACore) extends CoreTesterBase(c) {
+  var gvt = 10
   var lp_id = 5
   var time = 17
 
   var hist_cnt = 1
   val msg = HistMsg(time = 13, target_time = 27, target_lp = 9, cancel = 0)
 
-  setGvt(10)
+  setGvt(gvt)
   waitUntilEvtReq()
   issueEvent(lp_id, time)
   1 to 3 foreach { _ => takestep() }
-  setHistCnt(1)
+  setHistCnt(hist_cnt)
 
   val rd_req = waitForHistRdReq(lp_id, hist_cnt)
   1 to 5 foreach { _ => takestep() }
@@ -148,20 +175,203 @@ class SimpleCoreTester(c: PDESACore) extends CoreTesterBase(c) {
   takestep()
   takestep()
 
-  sendHistMsg(w1, HistMsg(0, 0, 0, 0))
-  sendHistMsg(w2, HistMsg(0, 0, 0, 0))
+  sendHistMsg(w1)
+  sendHistMsg(w2)
 
   waitForEvtCompletion()
   takestep()
 }
 
+class CoreRollbackTester(c: PDESACore) extends CoreTesterBase(c) {
+  var gvt = 35
+  var lp_id = 5
+  var time = 51
+
+  var hist_cnt = 2
+  val msg1 = HistMsg(time = 39, target_time = 67, target_lp = 9, cancel = 0)
+  /* This event should be rolled back */
+  val msg2 = HistMsg(time = 53, target_time = 73, target_lp = 11, cancel = 0)
+
+  setGvt(gvt)
+  waitUntilEvtReq()
+  issueEvent(lp_id, time)
+  1 to 3 foreach { _ => takestep() }
+  setHistCnt(hist_cnt)
+
+  val rd_req = waitForHistRdReq(lp_id, hist_cnt)
+  1 to 5 foreach { _ => takestep() }
+  sendHistMsg(rd_req, msg1)
+  sendHistMsg(rd_req, msg2)
+
+  val gen_evt = waitForGenEvt(time)
+  val evt_msg = HistMsg(time, gen_evt.time, gen_evt.lp, gen_evt.cancel)
+
+  val cncl_evt = waitForCancelEvt(time = 73)
+  val rb_evt = waitForRollbackEvt(time = 53, lp = lp_id)
+
+  val w1 = waitForHistWrReq(msg1)
+  val w2 = waitForHistWrReq(evt_msg)
+
+  takestep()
+  takestep()
+
+  sendHistMsg(w1)
+  sendHistMsg(w2)
+
+  waitForEvtCompletion()
+  takestep()
+}
+
+class CoreMultiRollbackTester(c: PDESACore) extends CoreTesterBase(c) {
+  var gvt = 35
+  var lp_id = 5
+  var time = 51
+
+  var hist_cnt = 2
+  /* Both events should be rolled back */
+  val msg1 = HistMsg(time = 79, target_time = 97, target_lp = 9, cancel = 0)
+  val msg2 = HistMsg(time = 53, target_time = 73, target_lp = 11, cancel = 0)
+
+  setGvt(gvt)
+  waitUntilEvtReq()
+  issueEvent(lp_id, time)
+  1 to 3 foreach { _ => takestep() }
+  setHistCnt(hist_cnt)
+
+  val rd_req = waitForHistRdReq(lp_id, hist_cnt)
+  1 to 5 foreach { _ => takestep() }
+  sendHistMsg(rd_req, msg1)
+  sendHistMsg(rd_req, msg2)
+
+  val gen_evt = waitForGenEvt(time)
+  val evt_msg = HistMsg(time, gen_evt.time, gen_evt.lp, gen_evt.cancel)
+
+  val cncl_evt1 = waitForCancelEvt(time = 97)
+  val rb_evt1 = waitForRollbackEvt(time = 79, lp = lp_id)
+
+  val cncl_evt2 = waitForCancelEvt(time = 73)
+  val rb_evt2 = waitForRollbackEvt(time = 53, lp = lp_id)
+
+  val w1 = waitForHistWrReq(evt_msg)
+
+  takestep()
+  takestep()
+
+  sendHistMsg(w1)
+
+  waitForEvtCompletion()
+  takestep()
+}
+
+class CoreCancelTester(c: PDESACore) extends CoreTesterBase(c) {
+  var gvt = 35
+  var lp_id = 5
+  var time = 51
+
+  var hist_cnt = 2
+  val msg1 = HistMsg(time = 39, target_time = 97, target_lp = 9, cancel = 0)
+  /* This event is the pending anti-message */
+  val msg2 = HistMsg(time = 51, target_time = 0, target_lp = 0, cancel = 1)
+
+  setGvt(gvt)
+  waitUntilEvtReq()
+  issueEvent(lp_id, time)
+  1 to 3 foreach { _ => takestep() }
+  setHistCnt(hist_cnt)
+
+  val rd_req = waitForHistRdReq(lp_id, hist_cnt)
+  1 to 5 foreach { _ => takestep() }
+  sendHistMsg(rd_req, msg1)
+  sendHistMsg(rd_req, msg2)
+
+  //  val gen_evt = waitForGenEvt(time)
+  //  val evt_msg = HistMsg(time, gen_evt.time, gen_evt.lp, gen_evt.cancel)
+
+  val w1 = waitForHistWrReq(msg1)
+
+  takestep()
+  takestep()
+
+  sendHistMsg(w1)
+
+  waitForEvtCompletion()
+  takestep()
+}
+
+class CoreGVTTester(c: PDESACore) extends CoreTesterBase(c) {
+  var gvt = 45
+  var lp_id = 5
+  var time = 55
+
+  var hist_cnt = 2
+  /* This event is expired and will be erased from history */
+  val msg1 = HistMsg(time = 39, target_time = 67, target_lp = 9, cancel = 0)
+  val msg2 = HistMsg(time = 53, target_time = 73, target_lp = 11, cancel = 0)
+
+  setGvt(gvt)
+  waitUntilEvtReq()
+  issueEvent(lp_id, time)
+  1 to 3 foreach { _ => takestep() }
+  setHistCnt(hist_cnt)
+
+  val rd_req = waitForHistRdReq(lp_id, hist_cnt)
+  1 to 5 foreach { _ => takestep() }
+  sendHistMsg(rd_req, msg1)
+  sendHistMsg(rd_req, msg2)
+
+  val gen_evt = waitForGenEvt(time)
+  val evt_msg = HistMsg(time, gen_evt.time, gen_evt.lp, gen_evt.cancel)
+
+  val w1 = waitForHistWrReq(msg2)
+  val w2 = waitForHistWrReq(evt_msg)
+
+  takestep()
+  takestep()
+
+  sendHistMsg(w1)
+  sendHistMsg(w2)
+
+  waitForEvtCompletion()
+  takestep()
+}
+
+
 class CoreTest extends FreeSpec with Matchers {
-  "Core should generate a simple event" - {
-    "using interpreter" in {
-      chisel3.iotesters.Driver.execute(
-        Array("--backend-name", "verilator", "--fint-write-vcd"),
+  val options = Array("--backend-name", "verilator", "--fint-write-vcd")
+
+  "Core should process events properly" - {
+    "with simple event" in {
+      chisel3.iotesters.Driver.execute(options,
         () => new PDESACore(3, 8, 16)) { c =>
-        new SimpleCoreTester(c)
+        new CoreSimpleTester(c)
+      } should be(true)
+    }
+
+    "with single rollback" in {
+      chisel3.iotesters.Driver.execute(options,
+        () => new PDESACore(3, 8, 16)) { c =>
+        new CoreRollbackTester(c)
+      } should be(true)
+    }
+
+    "with multiple rollbacks" in {
+      chisel3.iotesters.Driver.execute(options,
+        () => new PDESACore(3, 8, 16)) { c =>
+        new CoreMultiRollbackTester(c)
+      } should be(true)
+    }
+
+    "with event cancelled" in {
+      chisel3.iotesters.Driver.execute(options,
+        () => new PDESACore(3, 8, 16)) { c =>
+        new CoreCancelTester(c)
+      } should be(true)
+    }
+
+    "with history cleared by GVT" in {
+      chisel3.iotesters.Driver.execute(options,
+        () => new PDESACore(3, 8, 16)) { c =>
+        new CoreGVTTester(c)
       } should be(true)
     }
   }
